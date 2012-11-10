9e42345d7f211be70e22ab463f59289f3d55ca39
unknown <allen@.(none)>
2012-11-10 20:19:00 +0800

    sdfsf

--- app/assets/html/基本信息_files/axurerp_pagespecificscript.js
+++
@@ -1,7 +0,0 @@
-for(var i = 0; i < 24; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
-
-$axure.eventManager.pageLoad(
-function (e) {
-
-});
-gv_vAlignTable['u3'] = 'top';gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u0'] = 'top';gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u19'] = 'center';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u9'] = 'top';gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u2'] = 'top';gv_vAlignTable['u11'] = 'top';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u7'] = 'center';
