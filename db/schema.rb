85c644f09e6f0a7cb28a6f05d6cf79508e365140
hodophobia <hodophobia@126.com>
2012-11-10 17:00:08 +0800

    devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,40 +11,129 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121024074127) do
+ActiveRecord::Schema.define(:version => 20121110084226) do
 
-  create_table "comments", :force => true do |t|
-    t.string   "commenter"
-    t.string   "body"
-    t.integer  "post_id"
+  create_table "apply_groups", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.integer  "owner_id"
+    t.integer  "status"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
 
-  add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.text     "descripion"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "follow_groups", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "follow_users", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "followed_user_id"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  create_table "group_categories", :force => true do |t|
+    t.integer "group_id"
+    t.integer "category_id"
+  end
+
+  add_index "group_categories", ["category_id", "group_id"], :name => "index_group_categories_on_category_id_and_group_id"
+  add_index "group_categories", ["group_id", "category_id"], :name => "index_group_categories_on_group_id_and_category_id"
+
+  create_table "group_reviews", :force => true do |t|
+    t.integer  "group_id_id"
+    t.text     "context"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "groups", :force => true do |t|
-    t.string   "title"
-    t.integer  "max_person"
-    t.string   "description"
+    t.string   "name"
+    t.text     "description"
+    t.string   "label"
+    t.string   "image_url"
+    t.integer  "owner"
+    t.boolean  "is_public"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
 
-  create_table "people", :force => true do |t|
-    t.string   "name"
-    t.integer  "age"
-    t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-  create_table "posts", :force => true do |t|
-    t.string   "name"
-    t.string   "title"
-    t.text     "content"
+  create_table "task_reviews", :force => true do |t|
+    t.integer  "task_id_id"
+    t.text     "context"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "tasks", :force => true do |t|
+    t.string   "name"
+    t.datetime "expect_begin_time"
+    t.datetime "expect_end_time"
+    t.datetime "begin_time"
+    t.datetime "end_time"
+    t.integer  "owner"
+    t.text     "description"
+    t.integer  "status"
+    t.integer  "assign_user"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+  end
+
+  create_table "user_groups", :force => true do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+  end
+
+  add_index "user_groups", ["group_id", "user_id"], :name => "index_user_groups_on_group_id_and_user_id"
+  add_index "user_groups", ["user_id", "group_id"], :name => "index_user_groups_on_user_id_and_group_id"
+
+  create_table "user_reviews", :force => true do |t|
+    t.integer  "user_id_id"
+    t.text     "context"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :id => false, :force => true do |t|
+    t.integer  "id",                                     :null => false
+    t.string   "name"
+    t.string   "email"
+    t.string   "QQ"
+    t.string   "weibo"
+    t.string   "phone"
+    t.integer  "gender"
+    t.string   "company"
+    t.string   "label"
+    t.string   "city"
+    t.string   "destrict"
+    t.string   "icon_url"
+    t.string   "position"
+    t.text     "description"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
