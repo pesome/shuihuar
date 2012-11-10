b89109177721bb84cb333f0b35f0c87785c8b853
unknown <allen@.(none)>
2012-11-11 03:29:54 +0800

    dsfsdf

---
+++ app/assets/javascripts/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j,k,[_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q)])]);}; 
+var b="rootNodes",c="pageName",d="List",e="type",f="Wireframe",g="url",h="List.html",i="Home",j="Home.html",k="children",l="Basic information",m="Basic_information.html",n="Team title",o="Team_title.html",p="Login",q="Login.html";
+return _creator();
+})();
