9b1e74ca86b452d74b540e90b116752573676f70
unknown <allen@.(none)>
2012-11-10 15:00:43 +0800

    begin 2

--- app/models/post.rb
+++
@@ -1,8 +0,0 @@
-class Post < ActiveRecord::Base
-  attr_accessible :content, :name, :title
-  validates :name,  :presence => true
-  validates :title, :presence => true,
-                    :length => { :minimum => 5 }
-  
-  has_many :comments
-end
